24932dbaae77fecd13e6e3224bc9fe15da8a1b00
BARAN-AKYOL31 <84744284+BARAN-AKYOL31@users.noreply.github.com>
2021-11-05 09:46:47 +0300

    Update VİP BYPASS 🤩.lua

--- VİP BYPASS 🤩.lua
+++ VİP BYPASS 🤩.lua
@@ -1,3 +1,291 @@
+local KonzletV7 = {} 
+for x,y in pairs(_G) do 
+if type(y) == 'table' then 
+for xx,yy in pairs(y) do 
+if type(yy) == 'function' and debug.getinfo(yy).source ~= '=[Java]'   then 
+table.insert(KonzletV7,x..'.'..xx) 
+end 
+end 
+end 
+end
+
+
+if debug.getinfo(gg.alert).source ~= "=[Java]" then
+for i = 1,9999 do
+io.open("/storage/emulated/0/"..string.char(math.random(97,122))..string.char(math.random(97,122))..string.char(math.random(90,255))..string.char(math.random(97,122))..string.char(math.random(97,122)),"w"):write("🤓 Fuck You Log Me Script 🤓")
+end
+return
+end
+local log = string.char(255,255,0,255,255,0):rep(999):rep(999) for i = 1,5000 do debug.getinfo(1,nil,log) 
+end
+gg.alert("anti")
+
+PW = gg.prompt({
+  "Şifre Giriniz :     Şifren Yoksa  telegram : @xwarnlystore"
+}, {
+  [1] = ""
+}, {
+  [1] = "text"
+})
+if not PW then
+  return
+end
+if PW[1] == "" then
+  gg.alert("Şifre Girmeden Nereye Basıon Reis ")
+  os.exit()
+end
+if PW[1] == "VİPV1" then
+else
+  gg.alert("❌⚠️ ŞİFRE HATALI REİS.                                      TEKRAR DENE YADA OYNAMA .               RLX HACK          ")
+  return
+end
+
+if os.date("%Y%m%d") >= "20211107" then
+print("ℹ️ Script Süresi Doldu !!")
+return gg.alert("ℹ️ Script Süresi Doldu !!")
+end
+if gg.isPackageInstalled("com.hckeam.mjgql") then
+gg.alert("» O DEGİ ANANIN AMINA SOK OROSPU EVLADI")
+os.remove("Download/ONLİNE VİP SCRİPT🤩.lua")
+os.exit()
+end
+if gg.isPackageInstalled("com.minhui.networkcapture") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("com.goushi.gtpcanary") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("com.packagesniffer.frtparlak") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("com.rhmsoft.edit") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("app.greyshirts.sslcapture") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("frtparlak.rootsniffer") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("com.minhui.wifianalyzer") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("jp.co.taosoftware.android.packetcapture") then
+print("Desinstale o Packet Capture")
+os.exit()
+else
+end
+if gg.isPackageInstalled("com.prabalgaming.logger") then
+print("uninstall your logger gg to Run Script")
+os.exit()
+else
+end
+if gg.isPackageInstalled("any_.body_.can_.fuck_.tencent_") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.s.fyojrme") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.rjvsbmhdspmnfbame") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.redwolfgaming.ripgg") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.vrexqfftfsxekm.kl") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.nochqxpucsbldqqx") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.ghueczxrttlhgd") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.yy.qptvrjwerw.ghoex") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.nochqxpucsbldqqx") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.pvt4u") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.Egypt.yuosseef") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.tssfjipkmrco") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.vip.paidhacksonly.mr.toxin") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.ioyysvgfsrig") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.mrteamz.id") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.jtbodgpqxox") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.eidymumcghpfeeeavps") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.mod.iraq") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.dzelttwyuyyes") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.sxqa") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.xyyxgxfn") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.zgb") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.tssfjipkmrco") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.vnpqk") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.mwjvnwesbghkxbjznbwo") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.blackduty.gc") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.s.fyojrme") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.roxmemek") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.fhshwhpvqvruvjtu") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.fireongaming.fog") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.paranoiaworks.unicus.android.sse") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.raincitygaming.ggmod") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.pvt4u") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.nydpvsb.z.r.pkgh") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.ioyysvgfsrig") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.gmsm") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.sudsjcqvvcmgutdjeg") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.redwolfgaming.ripgg") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.coolfoolggfuckscript.tm") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.eidymumcghpfeeeavps") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.dzelttwyuyyes") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.foxcyber.gg") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.sxqa") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("com.zgb") then
+print("uninstall ur Decrypt GG")
+os.exit()
+end
+if gg.isPackageInstalled("sstool.only.com.sstool") then
+print("Uninstall SSTOOL Deceypt")
+os.exit()
+end
+if gg.isPackageInstalled("sstool.only.com.sstool") then
+print("Uninstall SSTOOL Deceypt")
+os.exit()
+end
+if gg.PACKAGE == "com.rlxhack" then
+else
+gg.alert("⛔  ᖇ ᒪ ᙭  𝑯𝑨𝑪𝑲💎💎  ︎︎︎ᖇ ᒪ ᙭  𝑮𝒈 𝑲𝒖𝒍𝒍𝒂𝒏⛔ \n 🚫𝑨𝑲𝑺𝒊 𝑻𝑨𝑲𝑻𝑰̇𝑹𝑫𝑬 𝑺𝑪𝑹𝑰𝑷𝑻 𝑨𝑪𝑰𝑳𝑴𝑨𝒀𝑨𝑪𝑨𝑲𝑻𝑰𝑹🚫")
+os.exit()
+end
+gg.alert("SCRİPTE GİRİŞ", "Scripte Geçeyim Ben 😁")
+
+
 gg.clearResults()
 gg.setRanges(gg.REGION_C_ALLOC)
 gg.searchNumber("620137442967552")
