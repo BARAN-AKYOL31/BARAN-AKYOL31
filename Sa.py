befc2ebe1c43a62138cb5c02312afe4343f4df09
BARAN-AKYOL31 <84744284+BARAN-AKYOL31@users.noreply.github.com>
2022-03-14 14:20:29 +0300

    Sa.py

---
+++ Sa.py
@@ -0,0 +1,61 @@
+module.exports.run = async (bot, message, args) => {
+
+const v = require("../../bot.js")
+
+   a = v.editmsg(message,"**🍖🔥Sea**")
+
+    v.sleep(1000);
+
+   b = v.editmsg(message,a,"**S ve A❄️**")
+
+    v.sleep(1000);
+
+   c = v.editmsg(message,"**SEA🔥**")
+
+    v.sleep(1000);
+
+   d = v.editmsg(message,"**Selam Almayanın😡 **")
+
+    v.sleep(1000);
+
+   e = v.editmsg(message,"🌀Sea")
+
+    v.sleep(1000);
+
+   f = v.editmsg(message,"**🧿Slm**")
+
+    v.sleep(1000);
+
+   g = v.editmsg(message,"**🇹🇷Sa🇦🇿**")
+
+    v.sleep(1000);
+
+   h = v.editmsg(message,"**Selam🍁**")
+
+    v.sleep(1000);
+
+   ı = v.editmsg(message,"**❄️Naber**")
+
+    v.sleep(1000);
+
+   i = v.editmsg(message,"**Ben Geldim💬**")
+
+    v.sleep(1000);
+
+    v.sleep(1000);
+
+   j = v.editmsg(message,"**Hoşgeldim💋**")
+
+    v.sleep(1000);
+
+   k = v.editmsg(message, "**🚀🔥Sea**")
+
+    v.sleep(1000);
+
+    }
+
+module.exports.config = {
+
+    command: "sa"
+
+}
